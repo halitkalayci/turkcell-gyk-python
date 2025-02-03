690c60c6f351c8bf2096faeeac89e5d893c019b9
halitklyc <halitenes.kalayci@gmail.com>
2025-02-03 19:07:03 +0300

    constructor

--- oop.py
+++ oop.py
@@ -1,17 +1,22 @@
 print("oop")
 
 class Car:
-    name = "" #property-özellik (attribute-field)
-    def start(self):
-        print(f"{self.name} isimli araç başlatılıyor.")
+    # name = "" #property-özellik (attribute-field)
+    def __init__(self,name,model, year=None): # bir classın yapıcı bloğu-constructor
+        self.name = name
+        self.model = model
+        self.year = year
+        print("İnit çalıştı")
+        
+    def start(self, x, y):
+        print(f"{self.name} {self.model} {self.year} isimli araç başlatılıyor. {x} {y}")
+
     def stop(self): #(this) self -> classın kendisini ifade eder.
         print("Araç durduruluyor.")
 
-c1 = Car() # yeni instance oluşturma c1->instance (örnek)
-c1.name = "Hyundai"
-c1.start()
+c1 = Car("Hyundai","i20") # yeni instance oluşturma c1->instance (örnek)
+c1.start(1,2)
 c1.stop()
 
-c2 = Car()
-c2.name = "Honda"
-c2.start()
+c2 = Car("Honda","Civic") #yapıcı blok
+c2.start(2,3)
