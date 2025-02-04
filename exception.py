349f64b8ba5cdd6793a58e29da0cad4d74af28ba
halitklyc <halitenes.kalayci@gmail.com>
2025-02-04 19:06:45 +0300

    raise

--- exception.py
+++ exception.py
@@ -3,7 +3,7 @@
 print("Hata Yönetimi Başladı")
 
 # Kullanıcıdan 2 sayı al, bunların bölümünü print et.
-# try-catch-finally blokları -> try-except-finally-else
+# try-catch-finally blokları -> try-except-else-finally
 
 try:
     sayi1 = input("Lütfen ilk sayıyı giriniz: ") # casting "1", "abc","12345", "True", 1->"1"
@@ -12,14 +12,21 @@ try:
     sayi2 = input("Lütfen ikinci sayıyı giriniz: ") # casting "1", "abc","12345", "True", 1->"1"
     sayi2 = int(sayi2)
 
+    if(sayi2 == 0):
+        raise RuntimeError("İkinci sayı 0 olamaz.")
+        #throw
+
     print(f"Sonuç: {sayi1/sayi2}")
 
     print("Hata Yönetimi Bitti.")
     #...
-except ZeroDivisionError:
+except ZeroDivisionError as e:
+    print(e)
     print("İkinci sayı olarak 0 girilemez..")
-except:
-    print("Hata fırladı..")
+except RuntimeError as e:
+    print(e)
+except Exception as e:
+    print(f"Bilinmeyen hata...: {e}")
 else: # hiç bir hata fırlamazsa kullanılacak.
     print("Hata yok")
 finally:
